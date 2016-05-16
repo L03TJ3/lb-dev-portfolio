9a679d708f5ad401cf18c37e359389fa07709bd6
Laurence <laurenceweening@gmail.com>
2016-05-16 22:00:11 +0200

    Create new action/ add link to new page/ create route

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,13 @@
 class ProjectsController < ApplicationController
 
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home' => 'home#index'
   get 'projects' => 'projects#index'
+  get 'projects/new' => 'projects#new'
 
   root 'home#index'
 end
