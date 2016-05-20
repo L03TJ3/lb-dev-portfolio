5cb2e2b0f09804b9c4103b86dc1e6d9b47d36dce
l03tj3 <laurenceweening@gmail.com>
2016-05-20 16:51:40 +0200

    Add join table projects/tag, show tags with project

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,19 @@
 treehuggers = File.open(File.join(Rails.root,
                                   '/app/assets/images/treehuggers.png'))
-Project.create(
-  title: "Treehuggers",
-  description: "TreeHuggers was an teamproject during the second week of my
+projects = [[
+  "Treehuggers",
+  "TreeHuggers was an teamproject during the second week of my
                 traineeship at Codaisseur, where we were tasked to build an
                 AirBnB clone in Rails. We decided to sell luxury treehouses.
                 It was a good project to learn to work collaborative with
                 github. Also was it my first time working with a gem like devise
                 for authentication. Postgresql is used as an database backend
                 for storing the treehouses, users, and reservations.",
-  image: treehuggers,
-  github: "https://github.com/L03TJ3/treehouses",
-  heroku: "https://guarded-basin-47812.herokuapp.com"
-)
+  treehuggers,
+  "https://github.com/L03TJ3/treehouses",
+  "https://guarded-basin-47812.herokuapp.com",
+  "Ruby, Rails, PostgreSQL, Devise, Bootstrap",
+]]
 
 tags = [
   "Ruby",
@@ -21,6 +22,7 @@ tags = [
   "ReactJS",
   "Devise",
   "Materialize",
+  "Bootstrap",
   "jQuery",
   "Javascript",
   "CSS",
@@ -31,4 +33,17 @@ tags = [
 
 tags.each do |tag_name|
   tags = Tag.create( name: tag_name)
+end
+
+projects.each do | title, description, image, github, heroku, tagss |
+  project = Project.create( title: title, description: description, image: image, github: github, heroku: heroku)
+
+  tagss.split( ", ").each do |tag_name|
+    if tag = Tag.find_by( name: tag_name )
+    else
+      tag = Tag.create( name: tag_name )
+    end
+
+    ProjectsTags.create( tag_id: tag.id, project_id: project.id )
+  end
 end
